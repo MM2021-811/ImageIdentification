132ebf0bf7fa75cbd012b7c289d10ee71bc82b3a
libinsong <ziyunxiao@gmail.com>
2021-11-20 11:27:52 -0700

    update util

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 from tests.test1 import test_logger
-from util.load_f101 import create_food101_subset, load_data_to_vearch
 from util.vearchutil import VearchUtil
 from pprint import pprint
 
@@ -7,7 +6,6 @@ from pprint import pprint
 # load_data_to_vearch()
 
 util = VearchUtil(model_name="vgg16")
-image_name="./data/mm803/images/spaghetti_bolognese/restaurantb_54586.jpg"
-restaurant = "restaurantb"
-item = util.search_by_image(keyword=restaurant,image_name=image_name)
+image_name="./data/zerobox/images/blackbottles/000034.png"
+item = util.search_by_image(image=image_name)
 pprint(item)
--- util/vearchutil.py
+++ util/vearchutil.py
@@ -63,7 +63,7 @@ class VearchUtil:
 
         # logger.debug(data)
 
-        url = f"{self.server_url}/foods/foods/{uuid}"
+        url = f"{self.server_url}/bottle/bottle/{uuid}"
         # logger.debug(url)
         response = requests.post(url, json=data, headers=self.header)
 
@@ -76,7 +76,7 @@ class VearchUtil:
 
         return response.text
 
-    def search_by_image(self, keyword: str, image = None, feature: list = None) -> dict:
+    def search_by_image(self, keyword: str = None, image = None, feature: list = None) -> dict:
         """[summary]            
         Args:
             image ([type]): can be str: image file name or numpy.ndarray returned by cv2.read(image)
@@ -91,36 +91,58 @@ class VearchUtil:
 
         if feature is None:
             feature = self.extract_feature(image)
+        
+        if keyword is not None:
+            # have to use is_brute_search = 1
+            data = {
+                "query": {
+                    "filter": [
+                        {
+                            "term": {
+                                "operator": "and",
+                                "keyword": [keyword]
+                            }
+                        },
+                        {
+                            "term": {
+                                "operator": "and",
+                                "model_name": [self.vearchutil.model_name]
+                            }
+                        }
+                    ],
+                    "sum": [
+                        {
+                            "feature": feature,
+                            "field": "image"
+                        }
+                    ]
+                },
+                "is_brute_search": 1
+            }
+        else:
+            data = {
+                "query": {
+                    "filter": [
+                        {
+                            "term": {
+                                "operator": "and",
+                                "model_name": [self.vearchutil.model_name]
+                            }
+                        }
+                    ],
+                    "sum": [
+                        {
+                            "feature": feature,
+                            "field": "image"
+                        }
+                    ]
+                },
+                "is_brute_search": 1
+            }
 
-        # have to use is_brute_search = 1
-        data = {
-            "query": {
-                "filter": [
-                    {
-                        "term": {
-                            "operator": "and",
-                            "keyword": [keyword]
-                        }
-                    },
-                    {
-                        "term": {
-                            "operator": "and",
-                            "model_name": [self.vearchutil.model_name]
-                        }
-                    }
-                ],
-                "sum": [
-                    {
-                        "feature": feature,
-                        "field": "image"
-                    }
-                ]
-            },
-            "is_brute_search": 1
-        }
         s1 = json.dumps(data)
 
-        url = f"{self.server_url}/foods/foods/_search?size=10"
+        url = f"{self.server_url}/bottle/bottle/_search?size=10"
         # vearch api has limitation, must pass in as string other than dict
         response = requests.post(url, data=s1, headers=self.header)
 
@@ -129,12 +151,19 @@ class VearchUtil:
             raise VearchApiError(response.text)
 
         data = json.loads(response.text)
-        hits = data.get("hits").get("hits")
-        f_hit = hits[0]
-        item = dict()
-        item["score"] = f_hit.get("_score")
-        item["vearch_id"] = f_hit.get("_id")
-        item["data"] = f_hit.get("_source")
+        found_total = data.get("hits").get("total",0)
+        if(found_total > 0):
+            hits = data.get("hits").get("hits")
+            f_hit = hits[0]
+            item = dict()
+            item["score"] = f_hit.get("_score")
+            item["vearch_id"] = f_hit.get("_id")
+            item["data"] = f_hit.get("_source")
+        else:
+            item = item = dict()
+            item["score"] = 0
+            item["vearch_id"] = None
+            item["data"] = ""
 
         return item
 
@@ -147,7 +176,7 @@ class VearchUtil:
         Returns:
             api reponse.text
         """
-        url = f"{self.server_url}/foods/foods/{uuid}"
+        url = f"{self.server_url}/bottle/bottle/{uuid}"
         response = requests.delete(url, headers=self.header)
 
         logger.debug(response.status_code)
