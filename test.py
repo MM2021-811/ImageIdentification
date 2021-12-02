da06c7b6ea03b8d4eb34af1fdc78a78c1c4e8919
libinsong <ziyunxiao@gmail.com>
2021-12-01 19:09:10 -0700

    update

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from util.vearchutil import VearchUtil
 from util.testutil import TestUtil
 from pprint import pprint
 from util.load_bottles import load_data_to_vearch
-from init_vearch_local import create_space,delete_space
+from init_vearch_local import create_space,delete_space, create_db
 
 # create_food101_subset()
 # load_data_to_vearch()
@@ -31,9 +31,28 @@ def test_vit16():
 def test_load():
     load_data_to_vearch(model_name="vgg16")
 
-def test_alexnet():
-    delete_space("bottle","alexnet")
-    create_space("bottle","alexnet",feature_dim=256)
-    load_data_to_vearch(model_name="alexnet")
+def test_net(model_name = "alexnet"):
+    db_name="bottle"
+    create_db(db_name)
 
-test_alexnet()
+    image_name = "./data/zerobox/images/blackbottles/00001.png"
+    util = VearchUtil(model_name=model_name)
+    item = util.extract_feature(image=image_name)
+    feature_dim = len(item)
+    print(feature_dim)
+
+    delete_space("bottle",model_name)
+    create_space("bottle",model_name,feature_dim=feature_dim,partition=4)
+    load_data_to_vearch(model_name=model_name)
+
+def eval_net(model_name = "alexnet"):
+    testutil = TestUtil(model_name)
+    (accuracy, cmatrix,wrong_results) = testutil.test()
+
+    pprint(cmatrix)
+    pprint(f"Final accuracy: {accuracy}")
+    print("Wrong results:")
+    pprint(wrong_results)
+
+# test_net("vgg16")
+eval_net("vgg16")
